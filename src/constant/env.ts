041ddb5986c93994374cbcbcd08fce5b94cd3610
ThEditor <theditormcpe@gmail.com>
2023-06-17 21:09:27 +0530

    feat: initial nest commit

--- src/constant/env.ts
+++
@@ -1,9 +0,0 @@
-export const isProd = process.env.NODE_ENV === 'production';
-export const isLocal = process.env.NODE_ENV === 'development';
-export const showLogger = isLocal
-  ? true
-  : process.env.NEXT_PUBLIC_SHOW_LOGGER === 'true' ?? false;
-export const PORT = process.env.PORT;
-export const SECRET = process.env.SECRET;
-export const MONGODB_URI =
-  process.env.MONGODB_URI ?? 'mongodb://localhost:27017/';
