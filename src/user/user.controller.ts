019d57092900a7c7bfbf62891540fbabfa2352da
ThEditor <theditormcpe@gmail.com>
2023-06-20 15:08:27 +0530

    chore: getuser decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   @Get('me')
-  me(@Req() req) {
+  me(@GetUser() user: User) {
     return {
-      user: req.user,
+      user,
     };
   }
 }
